3b5a27794bc62d35a5cbac3d057ec9c44085149b
quankudo <quannguyen2004aa@gmail.com>
2025-03-04 21:23:15 +0700

    Init Interface IEmployee and Class Employee implement from IEmployee

---
+++ Oop_HuongDoiTuong/Oop_HuongDoiTuong/Employee.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Oop_HuongDoiTuong
+{
+    internal class Employee : IEmployee
+    {
+        private string name_332;
+        private int paymentPerHour_332;
+
+        public Employee()
+        {
+            
+        }
+
+        public Employee(string name_332, int paymentPerHour_332)
+        {
+            this.name_332 = name_332;
+            this.paymentPerHour_332 = paymentPerHour_332;
+        }
+
+        public int CaculateSalary()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetName()
+        {
+            return name_332;
+        }
+
+        public void SetName(string name_332)
+        {
+            this.name_332 = name_332;
+        }
+
+        public int GetPaymentPerHour()
+        {
+            return paymentPerHour_332;
+        }
+
+        public void SetPaymentPerHour(int paymentPerHour_332)
+        {
+            this.paymentPerHour_332 = paymentPerHour_332;
+        }
+    }
+}
---
+++ Oop_HuongDoiTuong/Oop_HuongDoiTuong/IEmployee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oop_HuongDoiTuong
+{
+    internal interface IEmployee
+    {
+        int CaculateSalary();
+        string GetName();
+    }
+}
